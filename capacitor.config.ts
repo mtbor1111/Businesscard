a001042ba4dfcbf5421f2868a3b3ad5a40233479
unknown <allegromtbor@gmail.com>
2022-11-10 11:22:01 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'codeswag-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
